7c592794bad303c19221e1378b073afc69d7d8f0
Lorenzo <lorenzofumi@gmail.com>
2025-02-10 17:10:40 +0100

    refactor: move notebooks to the appropriate folders

--- 2. Inpainting/inpaint.py
+++ 2. Inpainting/inpaint.py
@@ -1,36 +1,46 @@
+"""
+    Inpaint using OpenCV's inpainting function. Produces poor results.
+"""
+
 import os
 import cv2
 
-file_name = 'video.mp4'
-videos_path = 'videos'
-mask_path = 'results'
+file_name = "video.mp4"
+videos_path = "videos"
+mask_path = "results"
 mask_name = "mediapipe_mask.mp4"
 
 
 video = cv2.VideoCapture(os.path.join(videos_path, file_name))
 mask = cv2.VideoCapture(os.path.join(mask_path, mask_name))
-outputWriter = cv2.VideoWriter("results/inpaint_cv.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 30, (int(video.get(3)), int(video.get(4))), True)
+outputWriter = cv2.VideoWriter(
+    "results/inpaint_cv.mp4",
+    cv2.VideoWriter_fourcc(*"mp4v"),
+    30,
+    (int(video.get(3)), int(video.get(4))),
+    True,
+)
 
 while True:
     ret, frame = video.read()
     ret, mask_frame = mask.read()
-    
+
     if not ret:
         break
-    
-    cv2.imshow('frame', frame)
-    cv2.imshow('mask', mask_frame)
-    
+
+    cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask_frame)
+
     mask_frame = cv2.cvtColor(mask_frame, cv2.COLOR_BGR2GRAY)
     # result = cv2.inpaint(frame, mask_frame, 3, cv2.INPAINT_NS)
     result = cv2.inpaint(frame, mask_frame, 3, cv2.xphoto.INPAINT_FSR_BEST)
-    
-    cv2.imshow('result', result)
+
+    cv2.imshow("result", result)
     outputWriter.write(result)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
-    
+
 video.release()
 mask.release()
 outputWriter.release()
